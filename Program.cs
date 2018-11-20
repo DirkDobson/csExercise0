f09ca781c487fe779b3bdf30f92f994077a105a3
Dirk Dobson <dirkvancouver@live.com>
2018-11-20 15:23:06 -0700

    first exercise

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace csExercises0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput;
+            Console.WriteLine("put a number 1 - 10");
+            userInput = Console.ReadLine();
+            if (userInput <= 10 && >= 1) ;
+            {
+                Console.WriteLine("good");
+            }
+            else
+            {
+                Console.WriteLine("Not 1 - 10");
+            }
+        }
+    }
+}
