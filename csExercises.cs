be6145cc4376cf0a964a33ee27eac69108a55a16
Dirk Dobson <dirkvancouver@live.com>
2018-11-20 17:37:12 -0700

    greater of two number

---
+++ csExercises.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace csExercises0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("put a number 1 - 10");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            if (userInput >= 1 && userInput <= 10)
+            {
+                Console.WriteLine("good");
+            }
+            else
+            {
+                Console.WriteLine("bad");
+            }
+            Console.WriteLine("Put two number and I will tell you which is greater");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            var result = num1 > num2 ? $"{num1} is greater" : $"{num2} is greater";
+            Console.WriteLine(result);
+
+            Console.WriteLine("Height and Width landscape or portrait");
+
+        }
+    }
+}
